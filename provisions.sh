56e89c3923c1e22004574c233923d88f58423afb
Soham Chakraborty <chakraborty@pythian.com>
2018-02-19 19:41:58 +0530

    Adding packages

--- provisions.sh
+++ provisions.sh
@@ -2,3 +2,9 @@ ip addr add 192.168.10.10/24 dev enp0s8
 ip link set enp0s8 up
 systemctl disable firewalld.service
 systemctl stop firewalld.service
+yum -y localinstall https://fedorapeople.org/groups/katello/releases/yum/3.4/katello/el7/x86_64/katello-repos-latest.rpm
+yum -y localinstall https://yum.theforeman.org/releases/1.15/el7/x86_64/foreman-release.rpm
+yum -y localinstall https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
+yum -y localinstall https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum -y install foreman-release-scl python-django
+yum -y install katello
